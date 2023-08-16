122682e315cf7e390545197fe77439427644c50e
Geovanny Nasciment Silva <geovanny.gns1@gmail.com>
2023-08-15 22:25:14 -0300

    add userModel

--- src/controllers/user/createUser.js
+++ src/controllers/user/createUser.js
@@ -1,5 +1,21 @@
-const createUser = (req, res) => {
-    res.json({message: "User creado"});
+import user from '../../models/userModel.js'
+
+const createUser = async (req, res) => {
+    try {
+        const user = req.body;
+        const res = await user.create(req.body);
+        const [result] = res;
+        if(result.affectedRows === 1){
+            res.status(201).json({message: 'User created', 
+                user: {
+                    id: result.insertId,
+                }
+            });    
+        }
+    }catch (err) {
+        console.log(err);
+        res.status(500).json({message: 'Server error'});
+    }
 }
 
 export default createUser;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,7 +4,13 @@ const list = async () => {
     return await db.query('SELECT * FROM users');
 }
 
+const create = async (user) => {
+    const{name, email, pass} = user;
+    return await db.query('INSERTO INTO users (name, email, pass) VALUES (?,?,?)') [name, email, pass];
+}
+
 export default {
     list,
-
+    create,
+    
 }
